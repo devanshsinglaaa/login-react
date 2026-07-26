729b1c4f5dd690aa9061f6776bb6d9bf9cfc025c
devansh singla <devanshsinglaa@gmail.com>
2026-07-26 13:56:30 +0530

    Configure API URL using Vite environment variables

--- outstaff-app/src/pages/Login.jsx
+++ outstaff-app/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function Login() {
 
   const navigate = useNavigate();
@@ -21,7 +23,7 @@ export default function Login() {
 
     try {
 
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post(`${API_URL}/login`, {
         email,
         password,
         type: loginType,
